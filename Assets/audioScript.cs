a5f3b1d00e2f91208888a6e5bec053939550c9f9
Pablo Guallichico <pablomp250@gmail.com>
2020-01-30 17:05:23 -0500

    Code and figures base
    Base code and figures to future modifications

---
+++ Assets/audioScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class audioScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    // Start is called before the first frame update
+    public VirtualButtonBehaviour vbBtnObj;
+    public AudioSource fuente;
+    public AudioClip clip;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GetComponent<VirtualButtonBehaviour>();
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        fuente.clip = clip;
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        fuente.Play();
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        fuente.Stop();
+        Debug.Log("Button released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
